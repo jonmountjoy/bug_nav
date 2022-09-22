12a6630c86de2c42e239c7f8f392bbcb67cdf5fe
Jon Mountjoy <tech@jonmountjoy.com>
2022-09-22 16:19:18 +0100

    router + logging

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,139 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:logging/logging.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(App());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+/// The main app.
+class App extends StatelessWidget {
+  /// Creates an [App].
+  App({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  /// The title of the app.
+  static const String title = 'GoRouter Example: Navigator Observer';
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+  Widget build(BuildContext context) => MaterialApp.router(
+        routerConfig: _router,
+        title: title,
+      );
+
+  final GoRouter _router = GoRouter(
+    observers: <NavigatorObserver>[MyNavObserver()],
+    routes: <GoRoute>[
+      GoRoute(
+        // if there's no name, path will be used as name for observers
+        path: '/',
+        builder: (BuildContext context, GoRouterState state) =>
+            const Page1Screen(),
+        routes: <GoRoute>[
+          GoRoute(
+            name: 'page2',
+            path: 'page2/:p1',
+            builder: (BuildContext context, GoRouterState state) =>
+                const Page2Screen(name: "standard"),
+          ),
+          GoRoute(
+            name: 'page2alternative',
+            path: 'page2alternative/:p1',
+            pageBuilder: (BuildContext context, GoRouterState state) =>
+                const NoTransitionPage<void>(
+                    child: Page2Screen(name: "alternative")),
+          ),
+        ],
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+    ],
+  );
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+/// The Navigator observer.
+class MyNavObserver extends NavigatorObserver {
+  /// Creates a [MyNavObserver].
+  MyNavObserver() {
+    log.onRecord.listen((LogRecord e) => debugPrint('$e'));
   }
 
+  /// The logged message.
+  final Logger log = Logger('MyNavObserver');
+
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+  void didPush(Route<dynamic> route, Route<dynamic>? previousRoute) {
+    log.info('didPush: ${route.str}, previousRoute= ${previousRoute?.str}');
+  }
+
+  @override
+  void didPop(Route<dynamic> route, Route<dynamic>? previousRoute) =>
+      log.info('didPop: ${route.str}, previousRoute= ${previousRoute?.str}');
+
+  @override
+  void didRemove(Route<dynamic> route, Route<dynamic>? previousRoute) =>
+      log.info('didRemove: ${route.str}, previousRoute= ${previousRoute?.str}');
+
+  @override
+  void didReplace({Route<dynamic>? newRoute, Route<dynamic>? oldRoute}) =>
+      log.info('didReplace: new= ${newRoute?.str}, old= ${oldRoute?.str}');
+}
+
+extension on Route<dynamic> {
+  String get str => 'route(${settings.name}: ${settings.arguments})';
+}
+
+/// The screen of the first page.
+class Page1Screen extends StatelessWidget {
+  /// Creates a [Page1Screen].
+  const Page1Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(title: const Text(App.title)),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () => context.goNamed(
+                  'page2',
+                  params: <String, String>{'p1': 'pv1'},
+                  queryParams: <String, String>{'q1': 'qv1'},
+                ),
+                child: const Text('Go to page 2'),
+              ),
+              ElevatedButton(
+                onPressed: () => context.goNamed(
+                  'page2alternative',
+                  params: <String, String>{'p1': 'pv1'},
+                  queryParams: <String, String>{'q1': 'qv1'},
+                ),
+                child: const Text('Go to page 2 alternative'),
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+      );
+}
+
+/// The screen of the second page.
+class Page2Screen extends StatelessWidget {
+  final String name;
+
+  /// Creates a [Page2Screen].
+  const Page2Screen({required this.name, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(title: const Text(App.title)),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Welcome to Page 2 $name"),
+              const Text("You have arrived."),
+            ],
+          ),
+        ),
+      );
 }
